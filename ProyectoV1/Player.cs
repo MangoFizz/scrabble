7e35b4a9c70346061352b41c17a69b9020f5b1c1
aiwass <aiwass@BLOO>
2022-09-22 17:11:06 -0500

    Add project files.

---
+++ ProyectoV1/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoV1
+{
+    partial class Player
+    {
+        int idUsuario;
+        String username;
+        String password;
+
+        public Player(int idUsuario, String username, String password)
+        {
+            this.idUsuario = idUsuario;
+            this.username = username;
+            this.password = password;
+        }
+
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
