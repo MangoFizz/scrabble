47e3680a1532fddaf2b0e03f7cb4bb682f050c53
MangoFizz <ccs81.mr@gmail.com>
2022-09-26 18:15:13 -0500

    Add test for checkPassword method

--- Tests/AuthtenticatorTests.cs
+++ Tests/AuthtenticatorTests.cs
@@ -18,6 +18,16 @@ namespace Tests
         }
 
         [TestMethod]
+        public void testCheckPassword()
+        {
+            PrivateType authtenticator = new PrivateType(typeof(Authtenticator));
+            String password = "password";
+            String hashedPassword = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+            bool result = (bool)authtenticator.InvokeStatic("checkPassword", password, hashedPassword);
+            Assert.IsTrue(result);
+        }
+        
+        [TestMethod]
         public void TestLoginSuccess()
         {
             String username = "testUser";
