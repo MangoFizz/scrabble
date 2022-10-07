9da8286d08aee2792d9129ff825d7046c1e9927c
JerryBrick <ccs81.mr@gmail.com>
2022-10-07 16:53:21 -0500

    Restructure authenticator, add missing tests, add editor configs

---
+++ DataAccess/Authenticator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Data.Common;
+
+namespace DataAccess {
+    public class Authenticator {
+        public enum UserAuthResult {
+            Success = 0,
+            InvalidCredentials,
+            IncorrectPassword,
+            DatabaseError
+        }
+
+        public enum UserResgisterResult {
+            Success = 0,
+            UserAlreadyExists,
+            DatabaseError
+        }
+
+        public enum UserUnregisterResult {
+            Success = 0,
+            AuthFailed,
+            UserDoesNotExists,
+            DatabaseError
+        }
+
+        private static String hashPassword(String password) {
+            SHA256 sha256 = SHA256.Create();
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+
+            // Convert hash bytes to string in hex format
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < hashBytes.Length; i++) {
+                stringBuilder.Append(hashBytes[i].ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        private static bool checkPassword(String password, String hashedPassword) {
+            return hashPassword(password).Equals(hashedPassword);
+        }
+
+        public static UserAuthResult validateUser(String username, String password) {
+            try {
+                Scrabble99Entities context = new Scrabble99Entities();
+                var queryResult = from player in context.players where player.username == username select player;
+                if(queryResult.Count() > 0) {
+                    var user = queryResult.First();
+                    if(checkPassword(password, user.password)) {
+                        return UserAuthResult.Success;
+                    }
+                    return UserAuthResult.IncorrectPassword;
+                }
+                return UserAuthResult.InvalidCredentials;
+            }
+            catch(DbException ex) {
+                return UserAuthResult.DatabaseError;
+            }
+        }
+
+        public static UserResgisterResult registerUser(String username, String password) {
+            try {
+                Scrabble99Entities context = new Scrabble99Entities();
+                
+                if(context.players.Any(p => p.username == username)) {
+                    return UserResgisterResult.UserAlreadyExists;
+                }
+                
+                players player = new players();
+                player.username = username;
+                player.password = hashPassword(password);
+                context.players.Add(player);
+                context.SaveChanges();
+
+                return UserResgisterResult.Success;
+            }
+            catch(DbException ex) {
+                return UserResgisterResult.DatabaseError;
+            }
+        }
+
+        public static UserUnregisterResult unregisterUser(String username, String password) {
+            try {
+                Scrabble99Entities context = new Scrabble99Entities();
+                var queryResult = from player in context.players where player.username == username select player;
+                if(queryResult.Count() > 0) {
+                    if(validateUser(username, password) != UserAuthResult.Success) {
+                        return UserUnregisterResult.AuthFailed;
+                    }
+                    var player = queryResult.First();
+                    context.players.Remove(player);
+                    context.SaveChanges();
+                    return UserUnregisterResult.Success;
+                }
+                else {
+                    return UserUnregisterResult.UserDoesNotExists;
+                }
+            }
+            catch(DbException ex) {
+                return UserUnregisterResult.DatabaseError;
+            }
+        }
+
+    }
+}
---
+++ Tests/AuthenticatorTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess;
+using System;
+using System.Linq;
+
+using static DataAccess.Authenticator;
+
+namespace Tests {
+    [TestClass]
+    public class AuthenticatorTests {
+        // super secure password
+        private static String superPassword = "password";
+        private static String superPasswordHash = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+
+        // dummy users
+        private static String authTestsUser = "ImAUser";
+        private static String registerTestsUser = "tacosdemango";
+        private static String unregisterTestsUser = "tOdiomyriam";
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context) {
+            // Create dummy users
+            var dbContext = new DataAccess.Scrabble99Entities();
+            var player = new DataAccess.players();
+            player.password = superPasswordHash;
+
+            player.username = authTestsUser;
+            dbContext.players.Add(player);
+
+            player.username = unregisterTestsUser;
+            dbContext.players.Add(player);
+
+            dbContext.SaveChanges();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup() {
+            // Set up db context
+            var dbContext = new DataAccess.Scrabble99Entities();
+
+            Func<String, bool> unregisterDummyUser = username => {
+                try {
+                    var player = dbContext.players.Where(p => p.username == username).First();
+                    dbContext.players.Remove(player);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                catch(Exception ex) {
+                    return false;
+                }
+            };
+
+            if(!(unregisterDummyUser(authTestsUser) && unregisterDummyUser(registerTestsUser))) {
+                throw new Exception("failed to clean up");
+            }
+        }
+
+        [TestMethod]
+        public void testHashPassword() {
+            var authtenticator = new PrivateType(typeof(Authenticator));
+            String hashedPassword = authtenticator.InvokeStatic("hashPassword", superPassword) as String;
+            Assert.AreEqual(superPasswordHash, hashedPassword);
+        }
+
+        [TestMethod]
+        public void testCheckPassword() {
+            var authtenticator = new PrivateType(typeof(Authenticator));
+            var passwordCheckResult = (bool)authtenticator.InvokeStatic("checkPassword", superPassword, superPasswordHash);
+            Assert.IsTrue(passwordCheckResult);
+        }
+
+        [TestMethod]
+        public void testLoginSuccess() {
+            var loginResult = Authenticator.validateUser(authTestsUser, superPassword);
+            Assert.AreEqual(UserAuthResult.Success, loginResult);
+        }
+
+        [TestMethod]
+        public void testLoginInvalidCredentials() {
+            String nonexistentUser = "tacosdemango";
+            var loginResult = Authenticator.validateUser(nonexistentUser, superPassword);
+            Assert.AreEqual(UserAuthResult.InvalidCredentials, loginResult);
+        }
+
+        [TestMethod]
+        public void testLoginIncorrectPassword() {
+            String wrongPassword = "wrongPassword";
+            var loginResult = Authenticator.validateUser(authTestsUser, wrongPassword);
+            Assert.AreEqual(UserAuthResult.IncorrectPassword, loginResult);
+        }
+
+        [TestMethod]
+        public void testRegisterSuccess() {
+            String somePassword = "passwd123";
+            var registerResult = Authenticator.registerUser(registerTestsUser, somePassword);
+            Assert.AreEqual(UserResgisterResult.Success, registerResult);
+        }
+
+        [TestMethod]
+        public void testRegisterUserAlreadyExists() {
+            var registerResult = Authenticator.registerUser(authTestsUser, superPassword);
+            Assert.AreEqual(UserResgisterResult.UserAlreadyExists, registerResult);
+        }
+
+        [TestMethod]
+        public void testUnregisterSuccess() {
+            var unregisterResult = Authenticator.unregisterUser(unregisterTestsUser, superPassword);
+            Assert.AreEqual(UserUnregisterResult.Success, unregisterResult);
+        }
+
+        [TestMethod]
+        public void testUnregisterUserDoesNotExist() {
+            String nonexistentUser = "tacossinmango";
+            var unregisterResult = Authenticator.unregisterUser(nonexistentUser, superPassword);
+            Assert.AreEqual(UserUnregisterResult.UserDoesNotExists, unregisterResult);
+        }
+
+        [TestMethod]
+        public void testUnregisterAuthFailed() {
+            var unregisterResult = Authenticator.unregisterUser(authTestsUser, "wrongPassword");
+            Assert.AreEqual(UserUnregisterResult.AuthFailed, unregisterResult);
+        }
+    }
+}
