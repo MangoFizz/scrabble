cf1666dc13af6a5343dc7d9b55b301e74af31977
Angel <arno5roano@gmail.com>
2022-11-16 14:03:16 -0600

    Generate board and atril

--- Client/Game.xaml.cs
+++ Client/Game.xaml.cs
@@ -13,8 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing;
-
+using Color = System.Drawing.Color;
 
 namespace Client {
     /// <summary>
@@ -23,13 +22,35 @@ namespace Client {
     public partial class Game : Page {
         public Game() {
             InitializeComponent();
+            GenerateBoard();
+            GenerateArtilesPlayer1();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             App.Current.MainWindow.MainFrame.GoBack();
         }
 
-   
+
+        private void GenerateBoard() {
+            for(int i = 0; i < 81; i++) {
+                var ButtonCell = new Button();
+                ButtonCell.Width = 54;
+                ButtonCell.Height = 54;
+                ButtonCell.Background = new SolidColorBrush(Colors.LightGreen);
+                ButtonCell.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                BordGame.Children.Add(ButtonCell);
+            }
+        }
+
+        private void GenerateArtilesPlayer1() {
+            for(int i = 0; i < 7; i++) {
+                var ButtonLetter = new Button();
+                ButtonLetter.Width = 54;
+                ButtonLetter.Height = 54;
+                ButtonLetter.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                ArtilPlayer1.Children.Add(ButtonLetter);
+            }
+        }
     }
 
     
